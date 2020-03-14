e738f601d568cc0d23ec9cba8f01a06633421296
Josh Paige <paigejl@yahoo.com>
2020-03-14 14:14:26 -0400

    first commit

---
+++ Menu.py
@@ -0,0 +1,17 @@
+def print_menu():
+    print('-' * 80)
+    print('  The Snake Way - Welcome ')
+    print('-' * 80)
+
+    print('[1] Register New Item')
+    print('[2] Display Catalog')
+    print('[3] Show Out of Stock Items')
+    print('[4] Update Stock Manually')
+
+    print('[x] Exit')
+
+def print_header(title):
+    print('\n\n') # 2 blank lines
+    print('-' * 30)
+    print(title)
+    print('-' * 30)
---
+++ Warehouse.py
@@ -0,0 +1,154 @@
+"""
+Program: Warhouse inventory control system
+Functionality:
+    - Register items to the catalog
+        id (auto generate)
+        title
+        category
+        price
+        stock
+    -Display Catalog
+    -Display Catalog (items out of stock)
+    -Update the stock of a selected item
+
+"""
+from Menu import print_menu, print_header
+from Item import Item
+import os
+import pickle
+
+catalog = []
+id_count = 1
+data_file = 'catalog.data'
+
+# methods declaration
+
+def clear():
+    return os.system('cls')
+
+def save_catalog():
+    writer = open(data_file, "wb") # open a file to Write Binary
+    pickle.dump(catalog, writer)
+    writer.close()
+
+def read_catalog():
+    global data_file
+    global id_count
+
+    try:
+        reader = open(data_file, "rb") # open a file to Read Binary
+        temp_list = pickle.load(reader)
+
+        for item in temp_list:
+            catalog.append(item)
+    
+        last = catalog[-1] # get the last element from the array
+        id_count = last.id + 1
+
+        how_many = len(catalog)
+        print(" Loaded " + str(how_many) + " Items")
+    except:
+        # when the code above crashes, we get here
+        print(" *Error loading data!")
+
+def register_item():
+    global id_count # import the global variable into function scope
+    print_header('  Register new Item')
+    title = input('Please input the title: ')
+    category = input('Please input the Category: ')
+    price = float(input('Please input the Price: '))
+    stock = int(input('Please input the Stock: '))
+    # do validations here
+
+    # create the object
+   
+    new_item = Item() # <-- how to create an object of a class
+    new_item.id = id_count
+    new_item.title = title
+    new_item.category = category
+    new_item.price = price
+    new_item.stock = stock
+    
+    id_count += 1
+    catalog.append(new_item)
+    print("   Item created!")
+
+def display_catalog():
+    num_items = len(catalog) # <- get length of array or string
+    print_header(' Catalog contains ' + str(num_items) )
+    print(' | ID  | Title                | Category          |     Price |    Stock ')
+    print('-' * 80)
+
+
+
+    for item in catalog:
+        print(" | " + str(item.id).ljust(3) 
+            + " | " + item.title.ljust(20)
+            + " | " + item.category.ljust(17)
+            + " | " + str(item.price).rjust(9)
+            + " | " + str(item.stock).rjust(5) )
+    
+    print('-' * 80)
+
+
+def display_no_stock():
+    print_header(' Item Out of Stock ')
+    print('| Title              | Category          |     Price |    Stock ')
+    print('-' * 80)
+
+
+
+    for item in catalog:
+        if(item.stock == 0):
+            print(item.title.ljust(20)
+                + " | " + item.category.ljust(17)
+                + " | " + str(item.price).rjust(9)
+                + " | " + str(item.stock).rjust(5) )
+
+    print('-' * 80)
+
+def update_stock():
+    # show all the items
+    # ask the user to choose an id
+    # validate the selected id
+    # ask for the new stock value
+    #update the stock value of the selected items
+    display_catalog()
+    selected = int(input(' Please select the ID to update '))
+
+    found = False
+    for item in catalog:
+        if(item.id == selected):
+            new_stock = input(' Please input new stock value: ')
+            item.stock = int(new_stock)
+            print(' Id is correct ')
+            found = True
+            print(' Updated! ')
+    if(found == False):
+        print('** Error: Selected ID Does not exist, try again')
+    # here
+
+read_catalog()
+
+# loop to display menu
+
+opc = ''
+while(opc != 'x'):
+    clear()
+    print_menu()
+    opc = input('Select an option: ')
+
+    if(opc == '1'):
+        register_item()
+        save_catalog()
+    elif(opc == '2'):
+        display_catalog()
+    elif(opc == '3'):
+        display_no_stock()
+    elif(opc == '4'):
+        update_stock()
+        save_catalog()
+    elif(opc == '5'):
+        print_stock_value()
+
+    input('Press Enter to Continue... ')
